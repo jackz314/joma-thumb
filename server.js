e396e8d85c0f93d594ae1769df18b1b4cb07caa8
Ubuntu <ubuntu@ip-172-31-46-219.us-west-2.compute.internal>
2020-04-12 02:40:29 +0000

    asdfZZ

--- server.js
+++ server.js
@@ -310,15 +310,15 @@ app.use('/api', router);
 
 // FRONT END -------------------------------
 app.get('/', function(req, res) {
-    res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendfile('/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 app.get('/result', function(req, res) {
-    res.sendfile('./public/result.html'); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendfile('/public/result.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 app.get('/add', function(req, res) {
-    res.sendfile('./public/add.html'); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendfile('/public/add.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 
